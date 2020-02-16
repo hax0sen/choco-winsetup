af83db6ea584d37767acfd78d39d60dcb664d039
hax0sen <60272204+hax0sen@users.noreply.github.com>
2020-02-16 23:00:23 +0100

    changed some stuff

--- choco-package.bat
+++ choco-package.bat
@@ -1,5 +1,5 @@
 @echo off
-echo This will install chocolatey and other tools
+echo This will install chocolatey and other useful tools
 echo Browse https://chocolatey.org/packages for packages
 echo Ensure that your cmd.exe runs as Administrator
 echo .
@@ -8,18 +8,23 @@ echo .
 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n=allowGlobalConfirmation
 pause
-echo Now chocolatey is ready
+echo chocolatey is ready
 pause
 choco install git.install -y --params "/GitAndUnixToolsOnPath /NoAutoCrlf /WindowsTerminal"
 call refreshenv
 
-#web
+# Web
+// ------
 choco install firefox flashplayerplugin
 choco pin add -n=firefox
 choco install googlechrome
 choco pin add -n=googlechrome
+#plugins
+choco install lastpass-chrome
 
-#code
+
+# code & hacking 
+// ------
 choco install vscode
 choco pin add -n vscode
 choco install wireshark
@@ -31,21 +36,27 @@ choco install vagrant
 choco install cmder
 choco install hub
 choco install github-desktop
+choco install ccleaner
+
 
 # games
+// ------
 choco install steam 
 choco install origin
 choco install discord
 choco install teamspeak
 choco install vibrancegui
 
-# other tools 
+# other tools
+// ------ 
 choco install spotify
 choco install 7zip
 choco install vlc
 choco install keepass
+choco install lastpass
 
 ## Windows 10 Config
+// ------
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 goto END
